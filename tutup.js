a908093ba78cbf130903707e16d6cafcc871d394
clanblog <82025504+clanblog@users.noreply.github.com>
2021-07-27 13:21:30 +0700

    Update tutup.js

--- tutup.js
+++ tutup.js
@@ -1,6 +1,6 @@
-function myFunction4() {
+function myFunction3() {
   const myNodelist = document.querySelectorAll("img");
   for (let i = 0; i < myNodelist.length; i++) {
-    myNodelist[i].style.display = "block";
+    myNodelist[i].style.display = "none";
   }
 }
